108f1a7cc6e26bcd5550e9dd8431b4aca888ea2c
exceedololo <exceedololo@outlook.com>
2023-05-23 11:54:13 +0300

    changing to yegor;s key_value_storage

--- internal/repository/key_value_storage/key_value_storage.go
+++ internal/repository/key_value_storage/key_value_storage.go
@@ -1,25 +1,31 @@
 package key_value_storage
 
-import "sync"
+import (
+	"errors"
+)
 
-var storageMap = make(map[string]*int)
-var mutex = &sync.RWMutex{}
+var ErrKeyNotFound = errors.New("key not found")
 
-func GetValue(key string) *int {
-	mutex.RLock()
-	defer mutex.RUnlock()
-
-	value, ok := storageMap[key]
-	if !ok {
-		return nil
-	}
-
-	return value
+type Storage struct {
+	data map[string]int
 }
 
-func SetValue(key string, value int) {
-	mutex.Lock()
-	defer mutex.Unlock()
+func NewStorage() *Storage {
+	return &Storage{
+		data: make(map[string]int),
+	}
+}
 
-	storageMap[key] = &value
+func (s *Storage) Get(key string) (int, error) {
+
+	value, ok := s.data[key]
+	if !ok {
+		return 0, ErrKeyNotFound
+	}
+
+	return value, nil
+}
+
+func (s *Storage) Set(key string, value int) {
+	s.data[key] = value
 }
--- internal/usercases/incrementer/incrementer_service.go
+++ internal/usercases/incrementer/incrementer_service.go
@@ -3,17 +3,35 @@ package incrementer
 import (
 	"github.com/exceedololo/notion-/internal/models"
 	"github.com/exceedololo/notion-/internal/repository/key_value_storage"
-	//"notion-/internal/models"
 )
 
-func Increment(req models.KeyValueIncrementRequest) int {
-	currentValue := key_value_storage.GetValue(req.Key)
-	if currentValue == nil {
-		key_value_storage.SetValue(req.Key, req.Value)
-		return req.Value
+type IncrementerService struct {
+	storage key_value_storage.KeyValueStorage
+}
+
+func NewIncrementerService(storage key_value_storage.KeyValueStorage) *IncrementerService {
+	return &IncrementerService{
+		storage: storage,
+	}
+}
+
+func (s *IncrementerService) Increment(req models.KeyValueIncrementRequest) (int, error) {
+	currentValue, err := s.storage.Get(req.Key)
+	if err != nil && err != key_value_storage.ErrKeyNotFound {
+		return 0, err
 	}
 
-	newValue := *currentValue + req.Value
-	key_value_storage.SetValue(req.Key, newValue)
-	return newValue
+	var newValue int
+	if currentValue != nil {
+		newValue = *currentValue + req.Value
+	} else {
+		newValue = req.Value
+	}
+
+	err = s.storage.Set(req.Key, newValue)
+	if err != nil {
+		return 0, err
+	}
+
+	return newValue, nil
 }
